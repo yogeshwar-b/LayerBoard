6c7c6f5c548e3009be1dce120f22357fc7f94cb6
yogeshwar-b <byogeshwarreddy@gmail.com>
2023-05-18 15:19:55 +0530

    Boilerplate to start the development.

---
+++ src/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LayerBoard = () => {
+  return (
+      <h3>This is LayerBoard</h3>
+  );
+};
+
+export default LayerBoard;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import LayerBoard from './home';
+
+render(<LayerBoard />, document.getElementById('root'));
